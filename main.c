424046539639e718289c6d4667b3957b668025de
siahuat0727 <tansiahuat@gmail.com>
2018-12-13 22:18:57 +0800

    Add test

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "matrix.h"
 
+#define epsilon 1e-8
+
 void int_print(void *src)
 {
     printf("%d", *(int*)src);
@@ -22,6 +24,15 @@ void int_double_add(void *src1, void *src2, void *dst)
 }
 
 
+bool double_equal(void *src1, void *src2)
+{
+    double d1 = *(double*)src1;
+    double d2 = *(double*)src2;
+    if (d1 + epsilon < d2 || d2 + epsilon < d1)
+        return false;
+    return true;
+}
+
 int main()
 {
     MatrixOp mat_op = matrix_op_impl();
@@ -34,6 +45,10 @@ int main()
 
     Matrix o = mat_op.mul(m, n, int_double_mul, int_double_add);
     mat_op.print(o, double_print);
+
+    Matrix ans = MATRIX(((double[2][2]){ {11.00, 13.00}, {27.50, 32.50}, }));
+    bool is_equal = mat_op.equal(o, ans, double_equal);
+    puts(is_equal ? "Correct!" : "Wrong!");
 
     return 0;
 }
